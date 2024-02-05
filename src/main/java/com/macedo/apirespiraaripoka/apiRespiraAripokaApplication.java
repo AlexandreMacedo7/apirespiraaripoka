ba7e2f1930e36d5c2b351b2f7f74a50673bc6d2f
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-05 19:54:03 -0400

    Projeto Criado

---
+++ src/main/java/com/macedo/apirespiraaripoka/apiRespiraAripokaApplication.java
@@ -0,0 +1,13 @@
+package com.macedo.apirespiraaripoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class apiRespiraAripokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(apiRespiraAripokaApplication.class, args);
+	}
+
+}
