cae99d7cda03b650ef67e0eb331f6b75a1c1a4c0
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-06 11:41:08 -0400

    Cria dto de Denuncia para criação

---
+++ src/main/java/com/macedo/apirespiraaripoka/entity/dto/CriarDenunciaDtoRequest.java
@@ -0,0 +1,14 @@
+package com.macedo.apirespiraaripoka.entity.dto;
+
+import com.macedo.apirespiraaripoka.entity.TipoDenuncia;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CriarDenunciaDtoRequest(
+                @NotBlank(message = "Campo não pode ser nulo") String endereco,
+                String coordenadasGeograficas,
+                @NotNull(message = "Campo não pode ser nulo") TipoDenuncia tipoDenuncia,
+                @NotBlank(message = "Campo não pode ser nulo") String descricao) {
+
+}
