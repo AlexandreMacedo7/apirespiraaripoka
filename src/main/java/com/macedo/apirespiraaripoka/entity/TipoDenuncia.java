1d9f7c3a720f6a579ebf4775d0d2b7ced81c9e74
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-05 20:33:10 -0400

    Cria enum de Tipo de Denuncia

---
+++ src/main/java/com/macedo/apirespiraaripoka/entity/TipoDenuncia.java
@@ -0,0 +1,12 @@
+package com.macedo.apirespiraaripoka.entity;
+
+public enum TipoDenuncia {
+
+    QUEIMADA_DE_LIXO_DOMESTICO,
+    DESMATAMENTO_URBANO,
+    DESMATAMENTO_RURAL,
+    QUEIMADA_URBANA,
+    QUEIMADA_RURAL,
+    POLUICAO_DE_AGUAS
+
+}
