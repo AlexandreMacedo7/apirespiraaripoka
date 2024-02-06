af7d54c9a583b24a446ec5f866186362f892f8d8
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-06 11:38:58 -0400

    Cria interface de Controller Denuncia

---
+++ src/main/java/com/macedo/apirespiraaripoka/controller/DenunciaInterface.java
@@ -0,0 +1,20 @@
+package com.macedo.apirespiraaripoka.controller;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.macedo.apirespiraaripoka.entity.dto.CriarDenunciaDtoRequest;
+import com.macedo.apirespiraaripoka.entity.dto.DenunciaDtoResponse;
+
+public interface DenunciaInterface {
+
+    ResponseEntity<DenunciaDtoResponse> create(CriarDenunciaDtoRequest dtoRequest, UriComponentsBuilder uBuilder);
+
+    ResponseEntity<DenunciaDtoResponse> getDenunciaById(Long id);
+
+    ResponseEntity<Page<DenunciaDtoResponse>> getAllDenuncia(Pageable pageable);
+
+    ResponseEntity<?> deleteById(Long id);
+}
