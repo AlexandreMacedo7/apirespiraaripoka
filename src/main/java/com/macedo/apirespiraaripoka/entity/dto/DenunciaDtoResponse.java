a9f3826fca5ab1656d6125d1b3a98f6595acc285
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-06 15:14:39 -0400

    Adiciona atributo para dto

--- src/main/java/com/macedo/apirespiraaripoka/entity/dto/DenunciaDtoResponse.java
+++ src/main/java/com/macedo/apirespiraaripoka/entity/dto/DenunciaDtoResponse.java
@@ -2,15 +2,17 @@ package com.macedo.apirespiraaripoka.entity.dto;
 
 import java.time.ZonedDateTime;
 
+import com.macedo.apirespiraaripoka.util.enums.StatusDenuncia;
 import com.macedo.apirespiraaripoka.util.enums.TipoDenuncia;
 
 public record DenunciaDtoResponse(
 
-        Long id,
-        ZonedDateTime dateTime,
-        String endereco,
-        String coordenadasGeografica,
-        TipoDenuncia tipoDenuncia,
-        String descricao) {
+                Long id,
+                ZonedDateTime dateTime,
+                String endereco,
+                String coordenadasGeografica,
+                TipoDenuncia tipoDenuncia,
+                String descricao,
+                StatusDenuncia status) {
 
 }
