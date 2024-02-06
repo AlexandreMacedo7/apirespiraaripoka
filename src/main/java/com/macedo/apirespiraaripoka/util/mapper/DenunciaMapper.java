1ea2f7cfbe6ed6eccb96e2e59bb8443bdb3de0ec
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-06 11:42:19 -0400

    Cria classe de Mapper para Denuncia

---
+++ src/main/java/com/macedo/apirespiraaripoka/util/mapper/DenunciaMapper.java
@@ -0,0 +1,32 @@
+package com.macedo.apirespiraaripoka.util.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.macedo.apirespiraaripoka.entity.Denuncia;
+import com.macedo.apirespiraaripoka.entity.dto.CriarDenunciaDtoRequest;
+import com.macedo.apirespiraaripoka.entity.dto.DenunciaDtoResponse;
+
+@Component
+public class DenunciaMapper {
+
+    public Denuncia toEntity(CriarDenunciaDtoRequest dtoRequest) {
+
+        Denuncia denuncia = new Denuncia(
+                dtoRequest.endereco(),
+                dtoRequest.coordenadasGeograficas(),
+                dtoRequest.tipoDenuncia(),
+                dtoRequest.descricao());
+
+        return denuncia;
+    }
+
+    public DenunciaDtoResponse toDto(Denuncia denuncia) {
+        DenunciaDtoResponse dtoResponse = new DenunciaDtoResponse(denuncia.getId(),
+                denuncia.getDateTime(),
+                denuncia.getEndereco(),
+                denuncia.getCoordenadasGeograficas(),
+                denuncia.getTipoDenuncia(),
+                denuncia.getDescricao());
+        return dtoResponse;
+    }
+}
