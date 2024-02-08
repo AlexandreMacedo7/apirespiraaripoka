4e7087f771e0818a3a2e8d809f7bcd13d0d82086
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-08 11:36:49 -0400

    Adiciona dto para retorno de dados de estatisticas

--- src/main/java/com/macedo/apirespiraaripoka/entity/dto/EstatisticaDenunciaDtoResponse.java
+++ src/main/java/com/macedo/apirespiraaripoka/entity/dto/EstatisticaDenunciaDtoResponse.java
@@ -2,10 +2,9 @@ package com.macedo.apirespiraaripoka.entity.dto;
 
 import java.util.Map;
 
-public record EstatisticaDenunciaDto(
+public record EstatisticaDenunciaDtoResponse(
     long totalDenuncias, 
     Map<String, Long> totalPorTipo,
     Map<String, Long> totalPorStatus
     ) {
-
 }
