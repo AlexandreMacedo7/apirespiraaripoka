6aac3a57cfe44fd70123782845a1bf9e19699556
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-17 14:54:21 -0400

    Cria método estático para consulta de status de Denuncia

--- src/test/java/com/macedo/demo/domain/builders/DenunciaDTOResponseBuilder.java
+++ src/test/java/com/macedo/demo/domain/builders/DenunciaDTOResponseBuilder.java
@@ -1,10 +1,12 @@
 package com.macedo.demo.domain.builders;
 
+import com.macedo.apirespiraaripoka.entity.dto.ConsultaStatusDenunciaDtoResponse;
 import com.macedo.apirespiraaripoka.entity.dto.CriarDenunciaDtoRequest;
 import com.macedo.apirespiraaripoka.entity.dto.DenunciaDetalhadaDtoResponse;
 import com.macedo.apirespiraaripoka.util.enums.StatusDenuncia;
 import com.macedo.apirespiraaripoka.util.enums.TipoDenuncia;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class DenunciaDTOResponseBuilder {
@@ -18,6 +20,15 @@ public class DenunciaDTOResponseBuilder {
                 TipoDenuncia.DESMATAMENTO_URBANO,
                 "Descrição válida",
                 StatusDenuncia.RECEBIDA
+        );
+    }
+
+    public static ConsultaStatusDenunciaDtoResponse consultaStatusDenunciaDtoResponse(){
+        return new ConsultaStatusDenunciaDtoResponse(
+                1L,
+                LocalDateTime.of(2024,1,1,06,30),
+                StatusDenuncia.RECEBIDA,
+                null
         );
     }
 }
