4807da514094f645992565c5545b9d95b01a0790
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-17 17:52:58 -0400

    Cria metodo estatico para novo status

--- src/test/java/com/macedo/demo/domain/builders/DenunciaDTORequestBuilder.java
+++ src/test/java/com/macedo/demo/domain/builders/DenunciaDTORequestBuilder.java
@@ -1,6 +1,8 @@
 package com.macedo.demo.domain.builders;
 
+import com.macedo.apirespiraaripoka.entity.dto.AtualizarStatusDenunciaDtoRequest;
 import com.macedo.apirespiraaripoka.entity.dto.CriarDenunciaDtoRequest;
+import com.macedo.apirespiraaripoka.util.enums.StatusDenuncia;
 import com.macedo.apirespiraaripoka.util.enums.TipoDenuncia;
 
 public class DenunciaDTORequestBuilder {
@@ -16,5 +18,9 @@ public class DenunciaDTORequestBuilder {
     public static CriarDenunciaDtoRequest denunciaDtoRequestInvalido() {
         return new CriarDenunciaDtoRequest("", "",
                 TipoDenuncia.QUEIMADA_DE_LIXO_DOMESTICO, "");
+    }
+
+    public static AtualizarStatusDenunciaDtoRequest novoStatusDenunciaDtoRequest(){
+        return new AtualizarStatusDenunciaDtoRequest(StatusDenuncia.ANALISANDO);
     }
 }
